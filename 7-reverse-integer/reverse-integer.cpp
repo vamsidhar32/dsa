fcc573052d7ebe3dc83d18172d0713329ec8474c
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-10 08:42:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (51.94%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int doop = x ;
+        int ans = 0 ;
+        while(x){
+            int lastdigit = x%10 ;
+            if(ans > INT_MAX /10 || ans < INT_MIN/10) return 0 ;
+            ans = ans * 10 + lastdigit ;
+            x= x/10;
+        }
+        
+        return ans ;
+    }
+};
