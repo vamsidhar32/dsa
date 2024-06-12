17c0dbc49b9278db2d4c5708cea6f1ef40aa4af2
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-12 13:06:29 +0530

    Time: 66 ms (67.67%) | Memory: 50.6 MB (21.53%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,16 +4,18 @@ public:
         int n = nums.size() ;
         int presum = 0 ;
         unordered_map<int,int> mpp ;
-        mpp[0] = 1 ;
         int count = 0 ;
-
+        mpp[0] = 1 ;
         for(int i = 0 ; i < n ; i++){
-            presum = presum + nums[i] ;
-            int rem = presum - k ;
-            count = count + mpp[rem] ;
-            mpp[presum]++ ;
-        }
+            presum += nums[i] ;
 
+            int rem = presum - k ; 
+            
+
+            count += mpp[rem];
+
+            mpp[presum]++;
+        }
         return count ;
     }
 };
