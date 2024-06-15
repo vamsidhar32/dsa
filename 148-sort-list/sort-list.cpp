b8bab9805f6724bddb5a56147af5657aac3ffcc7
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-15 19:26:26 +0530

    Time: 141 ms (34.76%) | Memory: 73.5 MB (37.14%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -9,25 +9,71 @@
  * };
  */
 class Solution {
+
 public:
-    ListNode* sortList(ListNode* head) {
-        vector<int>arr;
-
+    ListNode* middle(ListNode* head){
         if(head == NULL || head->next == NULL) return head ;
-        ListNode* temp = head ;
-
-        while(temp){
-            arr.push_back(temp->val);
-            temp = temp->next ;
+        ListNode* fast = head;
+        ListNode* slow = head ;
+        fast = fast->next->next ;
+        while(fast && fast->next){
+            fast = fast->next->next ;
+            slow = slow->next ;
         }
-
-        sort(arr.begin(),arr.end());
-        temp = head ;
-        for(int i =0 ; i <arr.size() ; i++){
-            temp->val = arr[i];
-            temp = temp->next ;
-        }
-
-        return head;
+        return slow ;
     }
+    ListNode* merge(ListNode* sort1, ListNode* sort2){
+        ListNode* first = sort1;
+        ListNode* second = sort2;
+    if(first == NULL) return second ;
+    if(second == NULL) return first;
+
+    ListNode* dummy = new ListNode(-1);
+
+    ListNode* prev = dummy ;
+
+    ListNode* l1 = first ;
+    ListNode* l2 = second; 
+
+    while(l1 && l2){
+        if(l1->val <= l2->val){
+            prev->next =l1 ;
+            prev = l1;
+            l1 = l1->next ;
+        }
+        else{
+            prev->next =l2 ;
+            prev = l2;
+            l2 = l2->next ;
+        }
+    }
+
+    if (l1) {
+        prev->next = l1;
+        prev = l1;
+    }
+
+    if(l2){
+        prev->next = l2;
+        prev = l2;
+    }
+
+    return dummy->next;
+
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head ;
+
+        ListNode* mid = middle(head);
+        ListNode* second = mid->next ;
+        mid->next = NULL ;
+
+        ListNode* sort1 = sortList(head);
+        ListNode* sort2 = sortList(second);
+
+        ListNode* merged = merge(sort1,sort2);
+
+        return merged;
+    }
+    
 };
