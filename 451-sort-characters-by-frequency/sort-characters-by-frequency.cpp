3d7ffd319ef9f31e9c1f0d506ec239d28122eebd
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-16 11:01:42 +0530

    Time: 12 ms (40.86%) | Memory: 11 MB (22.49%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        map<char,int>mp;
-        for(auto it:s){
-            mp[it]++;  //storing char and their freq
+        unordered_map<char,int>mpp;
+        
+        priority_queue<pair<int,char>> pq;
+        for(int i = 0 ; i < s.size() ; i++){
+            mpp[s[i]]++;
         }
-
-        priority_queue<pair<int,char>>pq;  //creating max heap forsorting map value based on feq 
-        for(auto it:mp){
+        for(auto it :mpp){
             pq.push({it.second,it.first});
         }
 
-        string om="";
+
+        stringstream ans ;
+
         while(!pq.empty()){
-            int n=pq.top().first;
-            char ch=pq.top().second;
+            pair<int,char> p = pq.top();
+
+            int n = p.first ;
+            char ch = p.second;
+            for(int i = 0 ; i <n ; i++)  ans << ch;
             pq.pop();
-            for(int i=0;i<n;i++){
-                om+=ch;
-            }
         }
-        return om;
+
+       
+
+        return ans.str() ;
+        
+
     }
 };
