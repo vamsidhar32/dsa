e260758e9aa42d80bfcf62f23def6988d313655e
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-16 11:02:45 +0530

    Time: 11 ms (52.61%) | Memory: 10.9 MB (31.33%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char,int>mpp;
+        map<char,int>mpp;
         
         priority_queue<pair<int,char>> pq;
         for(int i = 0 ; i < s.size() ; i++){
@@ -12,20 +12,20 @@ public:
         }
 
 
-        stringstream ans ;
+        string ans ="" ;
 
         while(!pq.empty()){
             pair<int,char> p = pq.top();
 
             int n = p.first ;
             char ch = p.second;
-            for(int i = 0 ; i <n ; i++)  ans << ch;
+            for(int i = 0 ; i <n ; i++)  ans += ch;
             pq.pop();
         }
 
        
 
-        return ans.str() ;
+        return ans;
         
 
     }
