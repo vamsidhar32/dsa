5030c41e41a5a8942f6372002b46063c93430313
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-16 10:11:19 +0530

    Time: 9 ms (51.26%) | Memory: 9.8 MB (19.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         unordered_map<char,int>mpp;
-
+        if(s.size() != t.size()) return false ;
         for(int i = 0 ; i <s.size(); i++){
             mpp[s[i]]++;
         }
         for(int i = 0 ; i<t.size(); i++){
+            if(mpp.find(t[i]) == mpp.end()) return false ;
             mpp[t[i]]--;
+            if(mpp[t[i]] < 0) return false ;
         }
-        for(auto it : mpp){
-            if(it.second != 0) return false ;
-        }
+        
 
         return true ;
     }
