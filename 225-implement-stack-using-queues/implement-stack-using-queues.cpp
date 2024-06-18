0dcc0699b9dbfb2d202721594952af95bce08dbb
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-18 09:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (40.14%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -6,15 +6,13 @@ public:
     MyStack() {}
     
     void push(int x) {
+        
+        q2.push(x);
         while(!q1.empty()){
             q2.push(q1.front());
             q1.pop();
         }
-        q1.push(x);
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }
+        swap(q1,q2);
     }
     
     int pop() {
