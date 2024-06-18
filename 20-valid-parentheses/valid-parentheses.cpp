0f3b8b30896da63a0966c06895b2d9353730f386
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-18 07:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (10.78%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        st.push('a');
+        map<char,char>mpp;
+        mpp[')'] = '(';
+        mpp['}'] = '{';
+        mpp[']'] = '[';
+        for(int i = 0 ; i <s.size(); i++){
+            if(s[i] == '(' || s[i] == '{' ||s[i] == '[' )
+                st.push(s[i]);
+            else{
+                if(st.top() == mpp[s[i]]) st.pop();
+                else return false ;
+            }
+        }
+        if(st.size() !=1) return false ;
+        return true ;
+    }
+};
