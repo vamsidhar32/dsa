a12b65f1cee2951f390f6c229c62cc716921f21c
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-20 19:32:01 +0530

    Time: 75 ms (25.39%) | Memory: 50.2 MB (5.06%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    #define mod 1000000007
+    void func1(vector<int> &ans, vector<int>&arr){
+        stack<pair<int,int>>st;
+
+        for(int i = arr.size()-1; i>=0; i--){
+            if(st.empty()){
+                ans.push_back(arr.size());
+                st.push({arr[i],i});
+            }
+            else if(arr[i] >= st.top().first){
+                ans.push_back(st.top().second);
+                st.push({arr[i],i});
+            }
+            else{
+                while(!st.empty() && arr[i] < st.top().first){
+                    st.pop();
+                }
+                if(st.empty()) ans.push_back(arr.size());
+                else ans.push_back(st.top().second);
+                st.push({arr[i],i});
+            }
+
+        }
+        reverse(ans.begin(),ans.end());
+
+    }
+
+
+
+
+    void func2(vector<int> &ans, vector<int>&arr){
+        stack<pair<int,int>>st;
+
+        for(int i = 0; i <arr.size(); i++){
+            if(st.empty()){
+                ans.push_back(-1);
+                st.push({arr[i],i});
+            }
+            else if(arr[i] > st.top().first){
+                ans.push_back(st.top().second);
+                st.push({arr[i],i});
+            }
+            else{
+                while(!st.empty() && arr[i] <= st.top().first){
+                    st.pop();
+                }
+                if(st.empty()) ans.push_back(-1);
+                else ans.push_back(st.top().second);
+                st.push({arr[i],i});
+            }
+
+        }
+    }
+
+
+    int sumSubarrayMins(vector<int>& arr) {
+        vector<int>ngli;
+        vector<int>ngri;
+
+        func1(ngri,arr);
+        func2(ngli,arr);
+        // for(int i = 0 ; i <arr.size(); i++){
+        //     cout << ngri[i] << " "  <<endl;
+        // }
+        // for(int i = 0 ; i <arr.size(); i++){
+        //     cout << ngli[i] << " "  <<endl;
+        // }
+
+        long long answer = 0 ;
+        for(int i = 0 ; i <arr.size(); i++){
+
+            int l = (i- ngli[i] )% mod;
+            int r = (ngri[i] - i) % mod ;
+
+            //cout << l << " " << r << " " ;
+            //sum = sum + arr[i] *l*r % mod ; 
+            //cout<<sum << " " << endl ;
+
+            answer += (long) (l) * (r) % mod * arr[i] % mod;
+            answer %= mod;
+
+        }
+        return answer ;
+
+    }
+    
+};
