bd29f6feb40e07fc6548f91ecb6a501db7e3ce88
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-22 05:55:20 +0530

    Time: 5 ms (43.80%) | Memory: 12.5 MB (82.65%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    void func(int index, vector<int>&temp,vector<vector<int>>&ans ,int target, vector<int> & candidates){
-        if(target == 0){
-            ans.push_back(temp);
-            return ;
+    void func(int index, vector<int>& temp, int& sumtemp, vector<int>& candidates, int target, vector<vector<int>>& ans) {
+        
+        if (sumtemp == target) ans.push_back(temp);
+        
+        if (index == candidates.size()) {
+            return;
         }
-        for(int i = index; i<candidates.size(); i++){
 
-            if(i>index && candidates[i]== candidates[i-1]) continue;
-            if(target < candidates[i]) break;
+        
+
+        for (int i = index; i < candidates.size(); i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) continue; // correctly skips duplicates
+            // early exit if the sum exceeds target
+            if (sumtemp > target) break;
+            sumtemp += candidates[i];
+            
             temp.push_back(candidates[i]);
-            func(i+1,temp,ans,target-candidates[i],candidates);
+            func(i + 1, temp, sumtemp, candidates, target, ans);
             temp.pop_back();
+            sumtemp -= candidates[i];
         }
-
-
-    }
     
+    }
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end()); // ensures the list is sorted to handle duplicates
 
-        sort(candidates.begin(),candidates.end());
-
-
-        vector<vector<int>>ans;
+        vector<vector<int>> ans;
         vector<int> temp;
+        int sumtemp = 0;
 
-        func(0,temp,ans,target,candidates);
+        func(0, temp, sumtemp, candidates, target, ans);
 
-        return ans ;
+        return ans;
     }
 };
