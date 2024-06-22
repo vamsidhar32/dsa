295d53b4746c039c5967787fa0e86a7139c61309
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-22 19:27:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (88.72%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -1,39 +1,29 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>ans ;
-        if(root == NULL){
-            return ans;
-        }
-        stack<TreeNode*>st1;
-        stack<int> st2;
-        st1.push(root);
-        while(!st1.empty()){
-            TreeNode* temp = st1.top();
-            st1.pop();
+        if (root == NULL) return {};
+        vector<int> ans;
+        stack<TreeNode*> st;
+        TreeNode* lastVisitedNode = nullptr;
+        TreeNode* curr = root;
 
-            st2.push(temp->val);
+        while (!st.empty() || curr != nullptr) {
+            if (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            } else {
+                TreeNode* topNode = st.top();
+                // If right node doesn't exist or it is already processed
+                if (topNode->right == nullptr || topNode->right == lastVisitedNode) {
+                    ans.push_back(topNode->val);
+                    st.pop();
+                    lastVisitedNode = topNode;
+                } else {
+                    curr = topNode->right;
+                }
+            }
+        }
 
-            if(temp->left) st1.push(temp->left);
-            if(temp->right) st1.push(temp->right);
-        }
-        while(!st2.empty()){
-            ans.push_back(st2.top());
-            st2.pop();
-        }
         return ans;
-        
     }
-    
 };
