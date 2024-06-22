56ddf3e13e32417f3f0272ee186b47b0fae63899
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-22 07:33:24 +0530

    Time: 3 ms (49.81%) | Memory: 7.7 MB (85.52%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void func(int index,int &tempsum ,vector<int>&temp,vector<vector<int>>&ans,int k , int n,vector<int>&nums){
-        if(tempsum >n) return ;
-        if(index == nums.size() ){
-            if(temp.size() == k && tempsum == n){
-                ans.push_back(temp);
-            }
-            return;
+    void func(int index,int sum ,vector<int>&temp,vector<vector<int>>&ans,int k,int n,vector<int>&nums){
+        if(index == 10) return ;
+
+        if(temp.size() == k && sum == n){
+            ans.push_back(temp);
         }
 
-        tempsum += nums[index];
-        temp.push_back(nums[index]);
-        func(index+1,tempsum,temp,ans,k,n,nums);
-        temp.pop_back();
-        tempsum -= nums[index];
-        func(index+1,tempsum,temp,ans,k,n,nums);
-
+        for(int i = index ; i < nums.size(); i++){
+            sum += nums[i];
+            temp.push_back(nums[i]);
+            func(i+1,sum,temp,ans,k,n,nums);
+            temp.pop_back();
+            sum-= nums[i];
+        }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>>ans;
-        vector<int>nums = {1,2,3,4,5,6,7,8,9};
         vector<int>temp ;
-        int tempsum = 0 ; 
-        func(0,tempsum,temp,ans,k,n,nums);
+        int sum = 0 ;
+        vector<int>nums = {1,2,3,4,5,6,7,8,9};
+
+        func(0,sum,temp,ans,k,n,nums);
+
         return ans ;
     }
 };
