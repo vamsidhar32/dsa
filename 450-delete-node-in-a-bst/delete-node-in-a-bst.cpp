98469997433a623a778fb0dffd180f964a811b28
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-23 21:11:08 +0530

    Time: 15 ms (96.72%) | Memory: 32 MB (78.52%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -10,74 +10,55 @@
  * };
  */
 class Solution {
-private:
-    TreeNode* helper(TreeNode* curr){
-        if(curr->left){
-            TreeNode* leftt = curr->left;
-            while(leftt->right){
-                leftt = leftt->right;
-            }
-            leftt->right = curr->right;
-            //delete curr ;
-            return curr->left ;
-        }
-        else if (curr->right){
-            TreeNode* rightt = curr->right;
-            while(rightt->left){
-                rightt = rightt->left;
-            }
-            rightt->left = curr->left ;
-            //delete curr ;
-            return curr->right ;
-        }
-        else {
-            //delete curr;
-            return NULL;
-        }
-
-        return NULL;
-    }
 public:
+    TreeNode* helper(TreeNode* root ){
+        // if(root->left == NULL && root->right == NULL){
+        //     delete root ;
+        //     return NULL ;
+        // } 
+        if(root->left == NULL) {
+            TreeNode* temp = root->right ;
+            delete root ;
+            return temp ;
+        }
+        if(root->right == NULL) {
+            TreeNode* temp = root->left;
+            delete root ;
+            return temp ;
+        }
+        TreeNode* cut =root->right ;
+        TreeNode* curr = root->left ;
+
+        while(curr->right){
+            curr = curr->right ;
+        }
+        curr->right = cut ;
+
+        TreeNode* temp = root->left ;
+        delete root ;
+        return temp;
+    }
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root == NULL){
-            return NULL ;
+        
+        TreeNode* curr = root ;
+        if(curr == NULL) return NULL;
+        if(curr->val == key){
+            return helper(curr);
         }
-        if(root->val == key){
-            return helper(root);
-        }
-        int flag = 0 ;
-        TreeNode* curr = root;
-        TreeNode* prev = NULL ;
         while(curr){
-            if(key > curr->val){
-                prev = curr;
-                curr = curr->right;
-                
-                flag = 1 ;
-                continue;
-            }
-
-            else if(key < curr->val){
-                prev = curr;
-                curr = curr->left;
-                
-                flag = -1 ;
-                continue;
-            }
-
-            else if(key == curr->val){
-                if(flag == 1 ){
-                    prev->right = helper(curr);
-                    break;
+            if(curr->val <key){
+                if(curr->right && curr->right->val == key){
+                    curr->right = helper(curr->right);
                 }
-                else if (flag == -1){
-                    prev->left = helper(curr);
-                    break;
+                curr = curr->right ;
+            }
+            else if (curr->val >key){
+                if(curr->left && curr->left->val == key){
+                    curr->left = helper(curr->left);
                 }
-          
+                curr = curr->left ;
             }
         }
         return root ;
     }
-
 };
