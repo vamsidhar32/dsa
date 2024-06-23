f1d7a9da9b88c61406ae5ec04bd498d948b391df
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-23 23:35:51 +0530

    Time: 8 ms (80.95%) | Memory: 26 MB (54.21%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -5,46 +5,29 @@
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr, right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-private:
-    TreeNode* build(vector<int>& inorder, vector<int>& postorder,int is,int ie, int ps, int pe , unordered_map<int,int> &mp){
-        if(is > ie || ps > pe){
-            return NULL ;
-        }
-        TreeNode* root = new TreeNode(postorder[pe]);
-
-        int num = mp[postorder[pe]];
-        int count = num - is;
-
-        root->left = build(inorder,postorder,is,num-1,ps,ps+count-1,mp) ;
-        root->right = build(inorder,postorder,num+1,ie,ps+count,pe-1,mp) ;
-
-        return root ;
+public:
+    TreeNode* buildtree(vector<int>& inorder, vector<int>& postorder, int ps, int pe, int is, int ie, unordered_map<int, int>& inmap) {
+        if (ps > pe || is > ie) return NULL;
+        TreeNode* node = new TreeNode(postorder[pe]);
+        int ind = inmap[node->val];
+        int diff = ind - is;
+        node->left = buildtree(inorder, postorder, ps, ps + diff - 1, is, ind - 1, inmap);
+        node->right = buildtree(inorder, postorder, ps + diff, pe - 1, ind + 1, ie, inmap);
+        return node;
     }
 
-public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int a = inorder.size();
-        int b = postorder.size();
-
-        if( a == 0 || b == 0 || a != b ){
-            return NULL ;
+        unordered_map<int, int> inmap;
+        for (int i = 0; i < inorder.size(); i++) {
+            inmap[inorder[i]] = i;
         }
-        unordered_map<int,int> mp;
-        for(int i = 0 ; i<a ; i++){
-            mp[inorder[i]] = i;
-        }
-        int is = 0 ;
-        int ie = a-1 ;
 
-        int ps = 0 ;
-        int pe = b-1 ;
-        TreeNode* root = build(inorder,postorder,is,ie,ps,pe,mp);
-
-        return root ;
+        int n = postorder.size();
+        return buildtree(inorder, postorder, 0, n - 1, 0, n - 1, inmap);
     }
 };
