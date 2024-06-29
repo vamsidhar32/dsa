e6e88c13858ebb2468a47faaf83ce37ba6608634
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-29 19:29:20 +0530

    Time: 18 ms (24.80%) | Memory: 19.7 MB (61.35%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -3,9 +3,9 @@ public:
 
     int changedp(int ind , int A , vector<int>&coins,vector<vector<int>>&dp){
 
-        if(A == 0) return 1 ;
+        
         if(ind ==0){
-            //nottakecase;
+            if(A == 0) return 1 ; //nottakecase;
             if(A % coins[ind] == 0) return 1 ;//takecase;
             else return 0 ;
         }
@@ -21,6 +21,24 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return changedp(n-1,amount,coins,dp);
+        //return changedp(n-1,amount,coins,dp);
+        for(int A = 0 ; A <= amount ; A++){
+            if(A == 0) dp[0][A] = 1 ;
+            if(A % coins[0] == 0) dp[0][A] = 1 ;//takecase;
+            else dp[0][A] = 0;
+        }
+
+        for(int ind = 1 ; ind <n ; ind++){
+            for(int A = 0 ; A<=amount ; A++){
+                int take = 0 ;
+                if(A >= coins[ind]) take = dp[ind][A-coins[ind]];
+                int nottake = dp[ind-1][A];
+
+
+                dp[ind][A] = take +nottake;
+            }
+            
+        }
+        return dp[n-1][amount];
     }
 };
