c43d0cc749bdfe9de63165b0afb158d31afc6bc0
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-29 06:01:16 +0530

    Time: 147 ms (56.48%) | Memory: 99.4 MB (22.36%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -3,11 +3,11 @@ public:
 
     bool find(int ind ,int sum , vector<int>nums,vector<vector<int>>&dp){
         
-
+        if(sum == 0 ) return dp[ind][sum] = true;
         if(ind == nums.size()) return false ;
 
         if(dp[ind][sum] != -1) return dp[ind][sum];
-        if(sum == 0 ) return dp[ind][sum] = true;
+        
         
 
         
@@ -15,7 +15,7 @@ public:
         if(sum-nums[ind] >=0) take = find(ind+1,sum-nums[ind],nums,dp);
         if(take) return dp[ind][sum] = true;
 
-        
+
         bool nottake = find(ind+1,sum,nums,dp);
         if(nottake) return dp[ind][sum] = true ;
 
@@ -28,6 +28,26 @@ public:
         if(sum %2 == 1) return false ;
         sum = sum/2;
         vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return find(0,sum,nums,dp);
+        
+
+        for(int i = 0 ; i <= n ;i++){
+            dp[i][0] = true ;
+        }
+
+        for(int i = 1 ; i <=sum ;i++){
+            dp[n][i] = false ;
+        }
+
+        for(int i = n-1 ; i >=0 ; i--){
+            for(int j = 1 ; j <=sum ;j++){
+                bool take = false ;
+                if( j- nums[i] >=0) take = dp[i+1][j-nums[i]];
+                bool nottake = dp[i+1][j];
+
+                dp[i][j] = take||nottake;
+            }
+        }
+
+        return dp[0][sum];
     }
 };
