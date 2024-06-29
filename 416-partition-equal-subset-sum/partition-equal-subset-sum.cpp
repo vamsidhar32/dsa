8c57b56f9c6ad05ee8ef4e6cb17112d92a401b3c
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-29 05:54:29 +0530

    Time: 1730 ms (5.02%) | Memory: 511.6 MB (5.00%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -4,16 +4,18 @@ public:
     bool find(int ind ,int sum , vector<int>nums,vector<vector<int>>&dp){
         
 
-        if(sum <0 || ind == nums.size()) return false ;
+        if(ind == nums.size()) return false ;
 
         if(dp[ind][sum] != -1) return dp[ind][sum];
         if(sum == 0 ) return dp[ind][sum] = true;
         
 
         
-
-        bool take = find(ind+1,sum-nums[ind],nums,dp);
+        bool take = false;
+        if(sum-nums[ind] >=0) take = find(ind+1,sum-nums[ind],nums,dp);
         if(take) return dp[ind][sum] = true;
+
+        
         bool nottake = find(ind+1,sum,nums,dp);
         if(nottake) return dp[ind][sum] = true ;
 
@@ -25,7 +27,7 @@ public:
         int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum %2 == 1) return false ;
         sum = sum/2;
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
         return find(0,sum,nums,dp);
     }
 };
