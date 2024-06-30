49b02990c4a3a8ae531bff2887123c6e0f7f8584
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-30 13:48:48 +0530

    Time: 6 ms (70.13%) | Memory: 11.9 MB (54.64%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -21,6 +21,29 @@ public:
         int m = word2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
 
-        return mindp(n-1,m-1,word1,word2,dp);
+        //return mindp(n-1,m-1,word1,word2,dp);
+
+        for(int i = 0 ; i <= n ; i++){
+            dp[i][0] = i ;
+        }
+
+        for(int j = 0 ; j <=m ; j++){
+            dp[0][j] = j ;
+        }
+
+        for(int i = 1 ;i <=n ; i++){
+            for(int j = 1 ; j <=m ; j++){
+                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+       
+                else{
+                    //insert
+                     dp[i][j] = min(1+dp[i][j-1],
+                            min(1+dp[i-1][j],
+                                   1+dp[i-1][j-1]));
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
