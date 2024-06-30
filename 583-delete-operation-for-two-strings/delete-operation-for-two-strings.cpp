0330bcaa6ff5e78233e500f28a64c89d82059741
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-30 12:25:10 +0530

    Time: 642 ms (5.02%) | Memory: 379.5 MB (5.12%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
 
-    
+    int mindp(int i , int j , string s1 , string s2, vector<vector<int>>&dp){
+
+
+        if(i <0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1[i] == s2[j]) return dp[i][j] = mindp(i-1,j-1,s1,s2,dp);
+        
+        else{
+            return dp[i][j] = 1+min(mindp(i-1,j,s1,s2,dp),mindp(i,j-1,s1,s2,dp));
+        }
+    }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
 
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
 
+        return mindp(n-1,m-1,word1,word2,dp);
         for(int i = 1 ; i <=n ; i++){
             for(int j = 1; j <=m ; j++){
                 if(word1[i-1] == word2[j-1]) dp[i][j] = 1+ dp[i-1][j-1];
