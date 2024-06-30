a4f34c0279b08850d9b6d9190ec890518a86e065
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-06-30 13:54:51 +0530

    Time: 9 ms (48.40%) | Memory: 12.6 MB (19.48%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -22,28 +22,31 @@ public:
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
 
         //return mindp(n-1,m-1,word1,word2,dp);
-
-        for(int i = 0 ; i <= n ; i++){
-            dp[i][0] = i ;
-        }
-
+        vector<int>prev(m+1,-1);
+        vector<int>curr(m+1,-1);
+        // for(int i = 0 ; i <= n ; i++){
+        //     dp[i][0] = i ;
+        // }
         for(int j = 0 ; j <=m ; j++){
-            dp[0][j] = j ;
+            prev[j] = j ;
         }
 
         for(int i = 1 ;i <=n ; i++){
+            curr[0] = i ;
+
             for(int j = 1 ; j <=m ; j++){
-                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                if(word1[i-1] == word2[j-1]) curr[j] = prev[j-1];
        
                 else{
                     //insert
-                     dp[i][j] = min(1+dp[i][j-1],
-                            min(1+dp[i-1][j],
-                                   1+dp[i-1][j-1]));
+                     curr[j] = min(1+curr[j-1],
+                            min(1+prev[j],
+                                   1+prev[j-1]));
                 }
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
