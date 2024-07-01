1ee8881fce7e47b2d9af8a9c94f5409290dd80ce
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-07-01 16:05:11 +0530

    Time: 566 ms (8.55%) | Memory: 294.7 MB (20.51%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -4,25 +4,42 @@ public:
         
         if(ind == nums.size()) return 0 ;
 
-        if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
+        if(dp[ind][prev] != -1) return dp[ind][prev];
         int take = 0;
-        if(prev == -1 || nums[prev] < nums[ind]){
+        if(prev == 0 || nums[prev-1] < nums[ind]){
 
-            take = 1+ lengthdp(ind+1,ind,nums,dp);
+            take = 1+ lengthdp(ind+1,ind+1,nums,dp);
             
         }
 
         int nottake = lengthdp(ind+1,prev,nums,dp);
 
 
-        return dp[ind][prev+1] = max(take,nottake);
+        return dp[ind][prev] = max(take,nottake);
 
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        int prev = -1;
+        
 
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return lengthdp(0,-1,nums,dp);
+        return lengthdp(0,0,nums,dp);
+
+
+        // for(int ind = n-1 ; ind>=0 ; ind++){
+        // for(int prev = 0 ; prev <=n ; prev++){
+        //     int take = 0;
+        //     if(prev == -1 || nums[prev] < nums[ind]){
+
+        //         take = 1+ lengthdp(ind+1,ind,nums,dp);
+            
+        //     }
+
+        //     int nottake = lengthdp(ind+1,prev,nums,dp);
+
+
+        //     dp[ind][prev+1] = max(take,nottake);
+        // }
+        // }
     }
 };
