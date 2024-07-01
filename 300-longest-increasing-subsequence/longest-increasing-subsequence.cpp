1cc81dd514651453852335238f5c50421f7dc19b
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-07-01 16:08:24 +0530

    Time: 503 ms (19.64%) | Memory: 294.4 MB (31.57%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -22,24 +22,26 @@ public:
         int n = nums.size();
         
 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return lengthdp(0,0,nums,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        //return lengthdp(0,0,nums,dp);
 
 
-        // for(int ind = n-1 ; ind>=0 ; ind++){
-        // for(int prev = 0 ; prev <=n ; prev++){
-        //     int take = 0;
-        //     if(prev == -1 || nums[prev] < nums[ind]){
+        for(int ind = n-1 ; ind >=0 ; ind--){
+        for(int prev = 0 ; prev < n ; prev++){
+            int take = 0;
+            if(prev == 0 || nums[prev-1] < nums[ind]){
 
-        //         take = 1+ lengthdp(ind+1,ind,nums,dp);
+                take = 1+ dp[ind+1][ind+1];
             
-        //     }
+            }
 
-        //     int nottake = lengthdp(ind+1,prev,nums,dp);
+            int nottake = dp[ind+1][prev];
 
 
-        //     dp[ind][prev+1] = max(take,nottake);
-        // }
-        // }
+            dp[ind][prev] = max(take,nottake);
+        }
+        }
+
+        return dp[0][0];
     }
 };
