8d338020ced82d850107028877bb471108b75c86
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-07-04 16:50:07 +0530

    Time: 169 ms (75.17%) | Memory: 47.5 MB (74.49%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int maxi(int ind , string s , unordered_set<string> &st,string temp){
-        if(ind == s.size() ) return 0;
-
-        temp = temp+s[ind];
-
-        int take = -1 ;
-        int nottake = -1 ;
-        if(st.find(temp) == st.end()){
-            st.insert(temp);
-            take = 1+maxi(ind+1,s,st,"");
-            st.erase(temp);
-        }
-        nottake = maxi(ind+1,s,st,temp);
-
-        return max(take,nottake);
-    }
     int maxUniqueSplit(string s) {
-
         int n = s.size();
-        //vector<int>dp(n+1,-1);
-        unordered_set<string>st;
-        string temp = "" ;
-        return maxi(0,s,st,temp);
+        unordered_set<string> st;
+        vector<int>dp(n+1,-1);
+        return dfs(s, 0, st,dp);
+    }
+    
+    int dfs(const string& s, int start, unordered_set<string>& st,vector<int>&dp) {
+        if (start == s.size()) {
+            return 0;
+        }
+        
+        int maxCount = 0;
+        string temp = "";
+        
+        for (int i = start; i < s.size(); ++i) {
+            temp += s[i];
+            if (st.find(temp) == st.end()) {
+                st.insert(temp);
+                maxCount = max(maxCount, 1 + dfs(s, i + 1, st,dp));
+                st.erase(temp);
+            }
+        }
+        
+        return maxCount;
     }
 };
