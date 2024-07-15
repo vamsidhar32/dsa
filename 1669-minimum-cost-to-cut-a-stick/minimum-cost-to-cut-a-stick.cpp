a2e0029c44649a6df2746ff9a31b9b9aaecce4dc
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-07-15 23:43:32 +0530

    Time: 24 ms (76.80%) | Memory: 12.5 MB (12.64%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -4,7 +4,7 @@ public:
 
     int mcmdp(int i , int j , vector<int>&cuts,vector<vector<int>>&dp){
 
-        if(i > j ) return 0 ;
+        if(i >= j ) return 0 ;
         
 
         if(dp[i][j] != -1) return dp[i][j];
@@ -29,10 +29,36 @@ public:
         
         int c = cuts.size();
 
-        vector<vector<int>> dp(c+1, vector<int>(c+1,-1));
+        vector<vector<int>> dp(c+2, vector<int>(c+2,0));
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
-        return mcmdp(1,c,cuts,dp);
+        //return mcmdp(1,c,cuts,dp);
+
+
+        for(int i = c ; i >= 1 ; i--){
+            for(int j = 1 ; j <=c ; j++){
+                    if(i > j ){ dp[i][j] = 0 ; continue ;}
+        
+
+       // if(dp[i][j] != -1) return dp[i][j];
+        int ans = 1e9;
+
+        
+        for(int k = i ; k <=j ; k++ ){
+
+            
+            int cost = cuts[j+1] - cuts[i-1] +dp[i][k-1] + dp[k+1][j];
+            
+    
+            ans  = min(ans,cost);
+        }
+
+
+        dp[i][j] = ans ;
+            }
+        }
+
+        return dp[1][c];
     }
 };
