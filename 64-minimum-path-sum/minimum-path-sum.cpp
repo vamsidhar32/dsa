d1b192e61ed915c05e2ed02fadd6c15b78f27c4b
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-07-21 05:21:43 +0530

    Time: 11 ms (18.59%) | Memory: 12.8 MB (13.76%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -19,19 +19,23 @@ public:
         int m = grid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
 
-        dp[n-1][m-1] = grid[n-1][m-1];
-        for(int i = n-2 ; i >=0 ;i--){
-            dp[i][m-1] = dp[i+1][m-1] + grid[i][m-1];
-        }
 
-        for(int j = m-2 ; j>=0 ; j--){
-            dp[n-1][j]= dp[n-1][j+1]+grid[n-1][j];
-        }
+        for(int i =n-1 ; i>=0 ; i--){
+            for(int j =m-1 ; j>=0 ; j--){
 
 
-        for(int j =m-2 ; j>=0 ; j--){
-            for(int i =n-2 ; i>=0 ; i--){
-                dp[i][j] =  grid[i][j] + min(dp[i+1][j],dp[i][j+1]);
+                if(i == n-1 && j == m-1){dp[i][j] = grid[i][j]; continue;}
+
+                //if(dp[i][j] != -1) return dp[i][j];
+
+                int leftsum = INT_MAX ;
+                if(i+1 < n) leftsum = grid[i][j] + dp[i+1][j];
+
+                int rightsum = INT_MAX ;
+                if(j+1 < m) rightsum = grid[i][j] + dp[i][j+1];
+
+                dp[i][j] = min(leftsum,rightsum);
+                //dp[i][j] =  grid[i][j] + min(dp[i+1][j],dp[i][j+1]);
             }
         }
 
