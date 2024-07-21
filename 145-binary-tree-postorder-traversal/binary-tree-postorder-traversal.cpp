f0a11e9ff293b80c04a57a6111bb49950b63b899
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-07-22 05:14:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (16.48%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -1,29 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        if (root == NULL) return {};
-        vector<int> ans;
-        stack<TreeNode*> st;
-        TreeNode* lastVisitedNode = nullptr;
-        TreeNode* curr = root;
-
-        while (!st.empty() || curr != nullptr) {
-            if (curr != nullptr) {
+        if(root == NULL) return {};
+        vector<int>ans ;
+        stack<TreeNode*>st;
+        //st.push(root);
+        TreeNode* curr = root;  
+        while(true){
+            while(curr){
                 st.push(curr);
-                curr = curr->left;
-            } else {
-                TreeNode* topNode = st.top();
-                // If right node doesn't exist or it is already processed
-                if (topNode->right == nullptr || topNode->right == lastVisitedNode) {
-                    ans.push_back(topNode->val);
+                curr= curr->left;
+            }
+            if(curr == NULL){
+                if(st.empty()) break;
+                if(st.top()->right == NULL){
+                    TreeNode* node = st.top();
+                    ans.push_back(st.top()->val);
                     st.pop();
-                    lastVisitedNode = topNode;
-                } else {
-                    curr = topNode->right;
+                    if(st.empty()) break;
+                    while(st.top()->right == node){
+                        node = st.top();
+                        ans.push_back(st.top()->val);
+                        st.pop();
+                        if(st.empty()) break;
+                    }
+                    if(st.empty()) break;
+                    curr = st.top()->right;
                 }
+                else curr = st.top()->right;
             }
         }
 
-        return ans;
+        return ans ;
     }
 };
