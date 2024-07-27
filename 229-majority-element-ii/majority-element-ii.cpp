0dae36fdbb4a33da35b4383f27ee5fb20c769cd7
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-07-28 01:34:46 +0530

    Time: 4 ms (91.34%) | Memory: 18.7 MB (33.39%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,43 +2,41 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int>ans ;
-        int n = nums.size();
-        int count1 = 0 ; 
-        int count2 = 0 ; 
-        int ele1 = INT_MIN ; int ele2 = INT_MIN ;
-        for(int i =0 ; i <nums.size() ; i++){
+        int ele1 = INT_MIN;
+        int ele2 = INT_MIN; 
+        int count1 = 0 ;
+        int count2 = 0 ;
 
+        for(int i = 0 ; i < nums.size() ; i++){
+            
             if(count1 == 0 && nums[i] != ele2){
                 ele1 = nums[i];
                 count1 = 1 ;
-            }
-
+            } 
             else if(count2 == 0 && nums[i] != ele1){
-                ele2 = nums[i] ;
-                count2 = 1;
+                ele2 = nums[i];
+                count2 = 1 ;
             }
             else if(nums[i] == ele1) count1++;
             else if(nums[i] == ele2) count2++;
-            else {
+
+            else{
                 count1--;
                 count2--;
             }
         }
 
-        int cnt1 = 0 ;
-        int cnt2 = 0 ;
-        for(int i = 0 ; i <n ; i++){
-            if(nums[i] == ele1) {
-                cnt1++;
-            }
-            if(nums[i] == ele2){
-                cnt2++;
-            }
+        int c = 0 , c2 = 0; 
+
+        for(int i =0 ; i < nums.size() ; i++){
+            if(nums[i] == ele1) c++;
+            if(nums[i] == ele2) c2++;
         }
+        int n = nums.size();
+        if(c>=(n/3)+1) ans.push_back(ele1);
+        if(c2>= (n/3)+1) ans.push_back(ele2);
 
-        if(cnt1 >= (n/3)+1) ans.push_back(ele1);
-        if(cnt2 >= (n/3)+1) ans.push_back(ele2);
 
-        return ans;
+        return ans ;
     }
 };
