6d802a45b36a909064e8ae8d22751e9be3604864
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-07-28 02:14:59 +0530

    Time: 42 ms (99.47%) | Memory: 27.5 MB (62.49%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<vector<int>> ans ;
-        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
 
-        for(int i = 0 ; i < n ; i++){
-
-            if(i !=0 && nums[i-1] == nums[i]) continue;
-            int j = i+1;
-            int k = n-1;
-
-            if(nums[i] > 0) break;
-            int sum = -(nums[i]);
-            
-
-            while(j<k){
+        for (int i = 0; i < nums.size(); i++) {
+            // Skip duplicates for nums[i]
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            int sum = -nums[i];
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
                 int value = nums[j] + nums[k];
-                if(value < sum){
+                if (value < sum) j++;
+                else if (value > sum) k--;
+                else {
+                    ans.push_back({nums[i], nums[j], nums[k]});
                     j++;
-                }
-                else if(value == sum){
-                    vector<int> temp ;
-                    temp.push_back(nums[i]);
-                    temp.push_back(nums[j]);
-                    temp.push_back(nums[k]);
-                    ans.push_back(temp);
-                    j++; k--;
-                    while(j<k && nums[j-1] == nums[j]) j++;
-                    while(j<k && nums[k] == nums[k+1]) k-- ;
-                }
-
-                else if(value > sum){
                     k--;
-                    
+                    // Skip duplicates for nums[j]
+                    while (j < k && nums[j] == nums[j-1]) j++;
+                    // Skip duplicates for nums[k]
+                    while (j < k && nums[k] == nums[k+1]) k--;
                 }
-            }    
+            }
         }
-        return ans ;
+
+        return ans;
     }
 };
