41deecaf938511dd6fb6c84a3dd9eef0cc9eb2a1
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-07-28 14:28:59 +0530

    Time: 12 ms (37.64%) | Memory: 9.8 MB (5.92%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,47 +1,53 @@
 class Solution {
 public:
-    int updatemax(map<char,int> &mpp){
+
+
+    int update(unordered_map<char,int>&mpp){
+
         int ans = 0 ;
         for(auto it : mpp){
             ans = max(ans,it.second);
         }
+
         return ans ;
     }
     int characterReplacement(string s, int k) {
+
         int n = s.size();
-
-        int i = 0 ;
-        int j = 0 ; 
-        int maxi = 0 ;
-        char store ;
+        int i =0 ; 
+        int j =0 ; 
         int ans = 0 ;
+
+        unordered_map<char,int>mpp;
+        int maxcount = 0 ;
         int count = 0 ;
-        map<char,int>mpp;
 
-        while(j<n){
-
+        while( j < n){
             mpp[s[j]]++;
             count++;
-            if(mpp[s[j]] > maxi){
-                maxi = mpp[s[j]];
-                //store = s[j];
+            if(mpp[s[j]] > maxcount){
+                maxcount = mpp[s[j]];
             }
 
-            if(count - maxi <= k){
-                ans =  max(ans,j-i+1);
+            if((count - maxcount) <=k){
+                ans = max(ans,j-i+1);
                 j++;
-            } 
+            }
 
-            else{
-                while(count-maxi >k){
+            else if((count - maxcount) >k ){
+                while((count - maxcount) > k){
+
+                    
                     mpp[s[i]]--;
-                    maxi = updatemax(mpp);
+                    maxcount = update(mpp);
                     count--;
+                    
                     i++;
+
                 }
                 j++;
             }
-            
+
         }
 
         return ans ;
