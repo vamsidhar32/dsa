ec63b4b07804cafe02ae67a7281296b60730f2cb
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-07-31 02:04:34 +0530

    Time: 4 ms (32.45%) | Memory: 14.6 MB (16.85%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -12,23 +12,28 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int>ans;
-        if(root == NULL) return ans;
-
-        queue<pair<TreeNode*,int>> q;
+        vector<int>ans ;
+        if(root == NULL) return  ans ;
+        queue<pair<TreeNode*,int>>q;
         q.push({root,0});
+
         int flag = 1 ;
+
         while(!q.empty()){
-            int s = q.size();
-            for(int i = 0 ; i <s ; i++){
-                pair<TreeNode*,int>p = q.front();
+            int size = q.size();
+
+            for(int i =0 ; i < size ; i++){
+                pair<TreeNode*,int>top= q.front();
                 q.pop();
-                if(flag == 1){ans.push_back(p.first->val);flag = 0 ;}
-                if(p.first->right) q.push({p.first->right,p.second+1});
-                if(p.first->left) q.push({p.first->left,p.second+1});  
+
+                if(flag == 1){ ans.push_back(top.first->val); flag = 0;}
+                if(top.first->right) q.push({top.first->right,top.second+1});
+                if(top.first->left) q.push({top.first->left,top.second+1});
+                
             }
             flag = 1 ;
         }
+
 
         return ans ;
     }
