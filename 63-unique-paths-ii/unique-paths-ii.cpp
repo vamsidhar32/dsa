ca0a2a6247fda8a216cb2f2ec1a51aa5145478a2
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-08-09 21:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (20.74%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
 
-    int pathdp(int i , int j ,vector<vector<int>> &obstacleGrid, int m ,int n ,vector<vector<int>>&dp){
+
+    int unidp(int i , int j , int n , int m , vector<vector<int>> &obstacleGrid , vector<vector<int>> &dp){
+        
+        
         if(obstacleGrid[i][j] == 1) return 0 ;
-        else if(i == n-1 && j == m-1 ) return 1 ;
+        if( i == n-1 && j == m-1) return 1 ;
+
         if(dp[i][j] != -1) return dp[i][j];
         int left = 0 ;
-        if(i+1 < n) left = pathdp(i+1,j,obstacleGrid,m,n,dp);
+        if(i+1 <= n-1) left = unidp(i+1,j,n,m,obstacleGrid,dp);
 
         int right = 0 ;
-        if(j+1 <m) right = pathdp(i,j+1,obstacleGrid,m,n,dp);
+        if(j+1 <= m-1) right = unidp(i,j+1,n,m,obstacleGrid,dp);
 
-        return dp[i][j] = left+right ;
-    }
+        return dp[i][j] =  left + right ;
+    } 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-
         int n = obstacleGrid.size();
         if(n == 0) return -1 ;
         int m = obstacleGrid[0].size();
 
-        //vector<vector<int>>dp(n,vector<int>(m,-1));
-        //return pathdp(0 , 0 , obstacleGrid,m,n,dp);
-        vector<long long >prev(m,1);
-        vector<long long >curr(m,1);
+        vector<vector<int>> dp(n,vector<int>(m,-1));
 
-        for(int i = n-1 ; i >=0 ; i--){
-            for(int j = m-1 ; j>=0 ; j--){
-                if(obstacleGrid[i][j] == 1) {curr[j] = 0 ; continue;}
-                else if(i == n-1 && j == m-1 ) {curr[j] = 1 ; continue ;}
-                //if(dp[i][j] != -1) return dp[i][j];
-                long long int left = 0 ;
-                if(i+1 < n) left = prev[j];
-
-                long long int right = 0 ;
-                if(j+1 <m) right = curr[j+1];
-
-                curr[j] = left+right ;
-                        
-            }
-            prev = curr;
-        }
-
-        return prev[0];
+        return unidp(0,0,n,m,obstacleGrid,dp);
     }
 };
