70f3da5a10eb4ea7878cd5d58e28cb68afcca0b0
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-08-09 21:49:10 +0530

    Time: 4 ms (73.45%) | Memory: 11.7 MB (19.61%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
 
-    int minidp(int i , int j , vector<vector<int>>&dp,int n,vector<vector<int>>&triangle){
-        if(i == n-1){
-            return triangle[i][j];
-        }
+    int mindp(int i , int j , int n , vector<vector<int>>&triangle,vector<vector<int>>&dp){
+        if( i == n-1) return triangle[i][j];
+
+
         if(dp[i][j] != -1) return dp[i][j];
-
-        int left = INT_MAX ;
-        if(i<n-1) left = triangle[i][j] + minidp(i+1,j,dp,n,triangle);
-
+        int left = INT_MAX;
+        if(i+1 <= n-1) left = triangle[i][j] + mindp(i+1,j,n,triangle,dp);
         int right = INT_MAX;
-        if(i<n-1) right = triangle[i][j] +minidp(i+1,j+1,dp,n,triangle);
+        if(i+1 <= n-1) right = triangle[i][j] + mindp(i+1,j+1,n,triangle,dp);
 
 
         return dp[i][j] = min(left,right);
@@ -19,8 +17,10 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
 
         int n = triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        
-        return minidp(0,0,dp,n,triangle);
+        int m = triangle[n-1].size();
+
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+
+        return mindp(0,0,n,triangle,dp);
     }
 };
