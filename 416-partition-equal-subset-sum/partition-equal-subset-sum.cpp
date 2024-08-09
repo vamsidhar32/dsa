c3696adf923e0ef10396ecac86c11b2252225593
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-08-10 00:00:37 +0530

    Time: 37 ms (95.23%) | Memory: 99.7 MB (19.75%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,53 +1,31 @@
 class Solution {
 public:
 
-    bool find(int ind ,int sum , vector<int>nums,vector<vector<int>>&dp){
-        
-        if(sum == 0 ) return dp[ind][sum] = true;
-        if(ind == nums.size()) return false ;
 
+    bool isdp(int ind , int sum , vector<int> &nums, vector<vector<int>>& dp){
+
+        if(sum == 0) return true ;
+        if(ind == 0){
+            if(nums[ind] == sum) return true ;
+            else return false ;
+        }
         if(dp[ind][sum] != -1) return dp[ind][sum];
-        
-        
+        bool take = false ;
+        if(sum - nums[ind] >=0) take = isdp(ind-1,sum - nums[ind] , nums,dp);
+        if(take) return dp[ind][sum] = true ;
 
-        
-        bool take = false;
-        if(sum-nums[ind] >=0) take = find(ind+1,sum-nums[ind],nums,dp);
-        if(take) return dp[ind][sum] = true;
-
-
-        bool nottake = find(ind+1,sum,nums,dp);
+        bool nottake = isdp(ind-1,sum,nums,dp);
         if(nottake) return dp[ind][sum] = true ;
 
         return dp[ind][sum] = false ;
     }
     bool canPartition(vector<int>& nums) {
-
         int n = nums.size();
         int sum = accumulate(nums.begin(),nums.end(),0);
-        if(sum %2 == 1) return false ;
+
+        if(sum %2 == 1) return false;
         sum = sum/2;
         vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        
-
-        for(int i = 0 ; i <= n ;i++){
-            dp[i][0] = true ;
-        }
-
-        for(int i = 1 ; i <=sum ;i++){
-            dp[n][i] = false ;
-        }
-
-        for(int i = n-1 ; i >=0 ; i--){
-            for(int j = 1 ; j <=sum ;j++){
-                bool take = false ;
-                if( j- nums[i] >=0) take = dp[i+1][j-nums[i]];
-                bool nottake = dp[i+1][j];
-
-                dp[i][j] = take||nottake;
-            }
-        }
-
-        return dp[0][sum];
+        return isdp(n-1,sum,nums,dp);
     }
 };
