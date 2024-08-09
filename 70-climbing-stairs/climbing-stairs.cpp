f8efcec150193a34a2499e558325e2d1279e20b6
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-08-09 15:31:16 +0530

    Time: 3 ms (29.51%) | Memory: 7.6 MB (20.43%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -6,7 +6,7 @@ public:
         if(ind == 2) return 2 ;
 
         if(dp[ind] != -1) return dp[ind];
-        
+
         int left = climbdp(ind-1,dp);
         int right = climbdp(ind-2,dp);
 
@@ -15,6 +15,24 @@ public:
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
         
-        return climbdp(n,dp);
+        //return climbdp(n,dp);
+        for(int ind = 1 ; ind <= n ; ind++){
+            if(ind == 1){
+                dp[ind] = 1 ;
+                continue ;
+            }
+            if(ind == 2) {
+                dp[ind] = 2 ;
+                continue ;
+            }
+            //if(dp[ind] != -1) return dp[ind];
+
+            int left = dp[ind-1];
+            int right =dp[ind-2];
+
+            dp[ind] = left+ right ;
+        }
+
+        return dp[n];
     }
 };
