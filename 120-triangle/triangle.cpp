f94c4a7f67d81cc18ead1827640e7db31696afb0
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-08-09 21:51:38 +0530

    Time: 4 ms (73.45%) | Memory: 11.7 MB (19.61%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -6,10 +6,10 @@ public:
 
 
         if(dp[i][j] != -1) return dp[i][j];
-        int left = INT_MAX;
-        if(i+1 <= n-1) left = triangle[i][j] + mindp(i+1,j,n,triangle,dp);
-        int right = INT_MAX;
-        if(i+1 <= n-1) right = triangle[i][j] + mindp(i+1,j+1,n,triangle,dp);
+        //int left = INT_MAX;
+        int left = triangle[i][j] + mindp(i+1,j,n,triangle,dp);
+        //int right = INT_MAX;
+        int right = triangle[i][j] + mindp(i+1,j+1,n,triangle,dp);
 
 
         return dp[i][j] = min(left,right);
@@ -22,5 +22,21 @@ public:
         vector<vector<int>> dp(n,vector<int>(m,-1));
 
         return mindp(0,0,n,triangle,dp);
+
+        for(int i = n-1 ; i >=0  ; i--){
+            for(int j = m-1 ; j>=0 ; j-- ){
+                if( i == n-1) dp[i][j] = triangle[i][j];
+
+
+                //if(dp[i][j] != -1) return dp[i][j];
+                int left = INT_MAX;
+                if(i+1 <= n-1) left = triangle[i][j] + mindp(i+1,j,n,triangle,dp);
+                int right = INT_MAX;
+                if(i+1 <= n-1) right = triangle[i][j] + mindp(i+1,j+1,n,triangle,dp);
+
+
+                return dp[i][j] = min(left,right);
+            }
+        }
     }
 };
