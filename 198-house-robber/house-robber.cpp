c0696c1087b53743be503df2fa74b8056a00d4f7
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-08-09 16:21:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (7.68%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
 
-    int rob (vector<int>&nums,int ind ,vector<int>&dp){
-        if(ind == 0 ) return nums[0];
-        if(ind == 1) return max(nums[0],nums[1]);
+    int robdp(int ind , vector<int> &nums , vector<int> &dp){
+        
+        if(ind == 0) return nums[0];
+        if(ind == 1) return max(nums[1],nums[0]);
 
-        if(dp[ind] != -1){
-            return dp[ind];
-        }
 
-        int left = nums[ind] + rob(nums,ind-2,dp);
-
-        int right = rob(nums,ind-1,dp);
+        if(dp[ind] != -1) return dp[ind] ;
+        
+        int left = nums[ind] + robdp(ind-2,nums,dp);
+        int right = robdp(ind-1,nums,dp);
 
         return dp[ind] = max(left,right);
+
     }
     int rob(vector<int>& nums) {
-
         int n = nums.size();
-        if(n ==0 ) return 0 ;
-        if(n == 1) return nums[0];
+
         vector<int>dp(n+1,-1);
-        dp[0] = nums[0];
-        dp[1] = max(nums[1],nums[0]);
-        for(int i = 2 ; i <n ; i++){
-            int left = nums[i] + dp[i-2];
-            int right = dp[i-1];
 
-            dp[i] = max(left,right);
-        }
-
-        return dp[n-1];
+        return robdp(n-1,nums,dp);
     }
 };
