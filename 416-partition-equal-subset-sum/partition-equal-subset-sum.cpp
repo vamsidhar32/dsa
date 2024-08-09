68f26b3c92c3186b7bb713bc8aa957b3204737e3
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-08-10 00:04:15 +0530

    Time: 180 ms (45.55%) | Memory: 99.7 MB (19.75%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -9,7 +9,10 @@ public:
             if(nums[ind] == sum) return true ;
             else return false ;
         }
+
         if(dp[ind][sum] != -1) return dp[ind][sum];
+
+
         bool take = false ;
         if(sum - nums[ind] >=0) take = isdp(ind-1,sum - nums[ind] , nums,dp);
         if(take) return dp[ind][sum] = true ;
@@ -25,7 +28,33 @@ public:
 
         if(sum %2 == 1) return false;
         sum = sum/2;
+
+        int k = sum ;
         vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return isdp(n-1,sum,nums,dp);
+        //return isdp(n-1,sum,nums,dp);
+
+        for(int ind = 0 ; ind <= n-1 ; ind++){
+            for(int sum = 0 ; sum <= k ; sum++){
+                if(sum == 0){dp[ind][sum] = true ; continue ;}
+                if(ind == 0){
+                    if(nums[ind] == sum){ dp[ind][sum] = true ; continue ;}
+                    else {dp[ind][sum] = false ; continue ;}
+                }
+
+                //if(dp[ind][sum] != -1) return dp[ind][sum];
+
+
+                bool take = false ;
+                if(sum - nums[ind] >=0) take = dp[ind-1][sum - nums[ind]];
+                if(take){dp[ind][sum] = true ; continue ;}
+
+                bool nottake = isdp(ind-1,sum,nums,dp);
+                if(nottake) {dp[ind][sum] = true ; continue ;}
+
+                dp[ind][sum] = false ;
+            }
+        }
+
+        return dp[n-1][k];
     }
 };
