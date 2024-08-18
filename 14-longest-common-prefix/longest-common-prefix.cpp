efc88e3739cf4ebb3f813889f98b044c9764cf96
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-08-18 05:54:34 +0530

    Time: 19 ms (5.99%) | Memory: 17.5 MB (8.51%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,27 +1,29 @@
-
 class Solution {
-private:
-    string findprefix(string s1 , string s2){
-        string ret ="";
-        int n = min(s1.length(),s2.length());
-        for(int i= 0;i<n;i++ ){
-            if(s1[i] == s2[i]){
-                ret.push_back(s1[i]);
-            }
-            else break;
-        }
-        return ret ;
-    }
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string longprefix = strs[0];
-        string prefix ;
-        for(int i= 0 ;i<strs.size();i++){
-            prefix = findprefix(longprefix,strs[i]);
-            longprefix = prefix ;
+    string prefixx(string s1, string s2){
+        string ans = "";
+        int i = 0 ;
+        int n1 = s1.size();
+        int n2 = s2.size();
 
+        //if(n1 == 0 || n2 == 0 ) return ans;
+
+        while(i< min(n1,n2)){
+            if(s1[i] == s2[i]){
+                ans = ans+s1[i];
+            }
+            else return ans ;
+            i++;
         }
-        return longprefix;
-        
+        return ans ;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+
+        string prefix = strs[0];
+
+        for(int i = 1 ; i<strs.size(); i++){
+            prefix = prefixx(prefix,strs[i]);
+        }
+        return prefix;
     }
 };
