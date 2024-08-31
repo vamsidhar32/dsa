987b3073863b8d181ed25f88e33ddd7defda78dc
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-08-31 15:47:37 +0530

    Time: 4 ms (33.85%) | Memory: 8.3 MB (36.31%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -17,12 +17,22 @@ public:
         return ans ;
     }
     
+    int ncr(int n ,int r ){
+        int value = 1 ;
+        int nume = n-1;
+        for(int i = 1 ; i < r ; i++){
+            value = value*nume;
+            value = value/i;
+        }
+
+        return value ;
+    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>ans ;
         for(int i = 1 ; i <= numRows ; i++){
             ans.push_back(genrow(i));
         }
-        //cout << ncr(6,3) << " " << ncr(7,4) << " " << ncr(7,3) ;
+        cout << ncr(6,3) << " " << ncr(7,4) << " " << ncr(7,3) ;
         return ans ;
 
     }
