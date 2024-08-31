a19ee4263b51cfd11b94d4cd24ef78b709b60f10
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-08-31 15:45:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (36.31%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
 
-
     vector<int> genrow(int row){
-        vector<int> ans ;
-        ans.push_back(1);
-        int nume = row-1;
+        vector<int>ans;
+
+        int nume = row-1 ;
         int value = 1 ;
+        ans.push_back(1);
         for(int i = 1 ; i < row ; i++){
-            value = value * nume ;
-            value = value /i;
+            value = value*nume ;
+            value = value/i;
             nume--;
             ans.push_back(value);
         }
 
         return ans ;
     }
-
-
-    int ncr(int n , int r){
-        int value = 1 ;
-        int nume = n-1;
-        for(int i = 1 ; i < r ; i++){
-            value = value*nume ;
-            value = value/i;
-            nume--;
-        }
-
-        return value ;
-    }
+    
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>ans;
-
+        vector<vector<int>>ans ;
         for(int i = 1 ; i <= numRows ; i++){
             ans.push_back(genrow(i));
         }
-        cout << ncr(6,3) << " " << ncr(7,4) << " " << ncr(7,3) ;
+        //cout << ncr(6,3) << " " << ncr(7,4) << " " << ncr(7,3) ;
         return ans ;
 
     }
