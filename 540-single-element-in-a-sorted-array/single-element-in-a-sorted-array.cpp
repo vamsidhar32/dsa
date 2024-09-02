afe87a061aebea02e8a1bfef5ab346dbdc7aba34
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-09-02 15:40:24 +0530

    Time: 26 ms (9.88%) | Memory: 24.9 MB (49.52%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -15,7 +15,7 @@ public:
             if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]){
                 return nums[mid];
             }
-            else if((mid % 2 == 1 && nums[mid] == nums[mid-1]) || (mid % 2 == 0 && nums[mid] == nums[mid+1])){
+            else if((mid % 2 == 1 && nums[mid] == nums[mid-1]) || (mid % 2 == 0 && nums[mid] != nums[mid-1])){
                 low = mid +1 ;
             }
             else high = mid -1 ;
