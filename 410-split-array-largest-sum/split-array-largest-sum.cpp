b3c2f61a8a4f5eb8371b66e2b64d28424c60421c
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-09-03 05:39:57 +0530

    Time: 4 ms (42.72%) | Memory: 9.4 MB (27.96%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
 
-    int func(vector<int>& nums, int mid , int k){
-
-        int subarrays = 0;
+    int func(vector<int> & nums, int mid , int k ){
+        int count = 1 ; 
         int sum = 0 ;
-        for(int i = 0 ; i< nums.size(); i++){
-            sum = sum+nums[i];
-            if(sum>mid){
-                subarrays++;
+        for(int i =0 ; i < nums.size() ; i++){
+            sum += nums[i];
+            if(sum > mid){
+                count++;
                 sum = nums[i];
+                if(count > k) return 0 ;
             }
         }
-        subarrays++;
-
-        if(subarrays <= k) return 1 ;
-        return 0;
+        if(count <= k) return 1 ;
+        return 0 ;
     }
     int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(),nums.end());
-
+        int n = nums.size();
+        
+        int low = *(max_element(nums.begin(),nums.end()));
         int high = accumulate(nums.begin(),nums.end(),0);
 
         while(low <= high){
             int mid = low + (high-low)/2;
 
             int comp = func(nums,mid,k);
-            cout << mid << " " << comp << " " << endl;
-            if(comp == 1){
-                high = mid-1;
-            }
-            else low = mid+1;
 
+            if(comp == 1) high = mid-1;
+            else low = mid+1 ;
         }
 
         return low ;
