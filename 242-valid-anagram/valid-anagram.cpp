37ae646d0594999a43a6bd604b2363d6fbecde21
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-09-04 11:49:04 +0530

    Time: 8 ms (63.46%) | Memory: 10 MB (15.74%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -7,7 +7,7 @@ public:
             mpp[s[i]]++;
         }
         for(int i = 0 ; i<t.size(); i++){
-            if(mpp.find(t[i]) == mpp.end()) return false ;
+            //if(mpp.find(t[i]) == mpp.end()) return false ;
             mpp[t[i]]--;
             if(mpp[t[i]] < 0) return false ;
         }
