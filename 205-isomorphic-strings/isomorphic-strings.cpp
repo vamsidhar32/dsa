ea02fc712da9fd4fddf1952c84be8c9e72829637
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-09-04 11:44:23 +0530

    Time: 11 ms (13.16%) | Memory: 9.7 MB (34.91%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size()) return false ;
+
+        int i =0 ;
         unordered_map<char,char>mpp;
-        unordered_set<char>st ;
-        int n1 = s.size();
-        int n2 = t.size();
-        if(n1!=n2) return false ;
-        int i = 0 ; 
-
-        while(i<n1){
+        unordered_map<char,char>mpp2;
+        while(i < s.size()){
             if(mpp.find(s[i]) == mpp.end()){
-                if(st.find(t[i]) == st.end()){
-                    mpp[s[i]] = t[i];
-                    st.insert(t[i]);
-                }
-                else return false ;
-                i++;
+                mpp[s[i]] = t[i];
             }
-
             else{
-                if(t[i] != mpp[s[i]]) return false ;
-                else i++;
+                if(mpp[s[i]] != t[i]) return false ;
+                if(mpp2[t[i]] != s[i]) return false ;
             }
-            
+            if(mpp2.find(t[i]) == mpp2.end()){
+                mpp2[t[i]] = s[i];
+            }
+            else{
+                if(mpp[s[i]] != t[i]) return false ;
+                if(mpp2[t[i]] != s[i]) return false ;
+            }
+            i++;
         }
 
         return true ;
     }
+
 };
