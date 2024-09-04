273b76f1f650062cbb352bb11f6f92658bf9626f
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-09-04 16:39:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (24.70%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -26,18 +26,13 @@ public:
             int k = s[i] - '0' ;
             ans = ans*10 + k ;
             i++ ;
-
-
-
-
         }
+
         if(flag == 0 && ans <= 2147483647){
             return ans ;
             
         }
         else if(ans > 2147483647 && flag == 0 ) return 2147483647 ;
-
-
         if( flag == 1  && ans <= 2147483648){
             return 0 - ans ;
         }
