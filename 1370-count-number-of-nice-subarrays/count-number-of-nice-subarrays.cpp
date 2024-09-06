a90a7ef9bdf7385487fe55a87f7f118ca8d7711c
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-09-06 06:41:08 +0530

    Time: 97 ms (47.79%) | Memory: 70.2 MB (84.75%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
 
-    int atmost(vector<int>&nums,int k){
-       // if(k == -1) return 0 ;
+    int func(vector<int> &nums,int k){
+        if(k == -1) return 0 ;
         int i = 0 ;
-        int j = 0 ; 
-        int n = nums.size();
-        int odd = 0 ;
+        int j = 0 ;
+        int odd  = 0 ;
         int count = 0 ;
-        while(j<n){
+
+        while( j < nums.size()){
             if(nums[j] % 2 == 1) odd++;
-            if(odd <=k){
-                count = count + (j-i+1);
+            
+            if(odd <=k ){
+                count += j-i+1;
                 j++;
             }
-            else if(odd >k){
-                while(odd >k){
+            else if(odd > k){
+                while(odd > k){
                     if(nums[i] %2 == 1) odd--;
                     i++;
                 }
-                count = count + (j-i+1);
+                count += j-i+1;
                 j++;
             }
         }
-
         return count ;
     }
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return atmost(nums,k) - atmost(nums,k-1);
+        return func(nums,k) - func(nums,k-1);
     }
 };
