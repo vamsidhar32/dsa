49a91cde00d2b53e9393e3cb683bff6c60fab63c
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-09-06 07:37:14 +0530

    Time: 25 ms (44.71%) | Memory: 10.8 MB (60.88%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,47 +1,51 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size() ;
-        int i = 0 ;
-        int j = 0 ;
-        int ans = 1e9; 
-        int first = 0 ;
-        int second = 0;
-        unordered_map<char,int>mpp;
-        for(int i = 0 ; i < t.size(); i++){
+        
+        int i =0 ;
+        int j =0 ;
+
+        int len = 1e9 ;
+        unordered_map<int,int>mpp;
+        for(int i = 0; i < t.size() ; i++){
             mpp[t[i]]++;
         }
         int count = mpp.size();
-        while(j < n){
+
+        int left= 0 ;
+        int right = 0 ;
+
+        while(j < s.size()){
             if(mpp.find(s[j]) != mpp.end()){
                 mpp[s[j]]--;
-                if(mpp[s[j]] == 0) count--;
-            } 
-            
-
+                if(mpp[s[j]] == 0) count-- ;
+            }
 
             if(count > 0){
                 j++;
             }
+
             else if(count == 0){
-           
                 while(count == 0){
-                    if(j-i+1 < ans){
-                        ans= j-i+1;
-                        first = i ;
-                        second = j;
+                    if( j-i+1 < len){
+                        left = i ;
+                        right = j ;
+                        len = j-i+1;
                     }
+
                     if(mpp.find(s[i]) != mpp.end()){
                         mpp[s[i]]++;
                         if(mpp[s[i]] == 1) count++;
                     }
-                    i++;    
+                    i++;
                 }
                 j++;
+                
             }
         }
 
-        if(ans == 1e9) return "";
-        return s.substr(first , second-first+1);
+        if(len == 1e9) return "";
+        return s.substr(left,right-left+1);
     }
+
 };
