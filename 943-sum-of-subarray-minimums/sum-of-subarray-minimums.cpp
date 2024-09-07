acf2cda95d2a31447fe74f8522f8e8572c4def52
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-09-07 07:28:43 +0530

    Time: 81 ms (11.17%) | Memory: 53.3 MB (5.05%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-
     #define mod 1000000007
-    void func1(vector<int> &arr, vector<int>&nsil){
+    void func1(vector<int>arr , vector<int> &nsil){
+
         stack<pair<int,int>>st;
-        for(int i = 0 ; i < arr.size() ; i++){
+        for(int i =0 ; i < arr.size() ; i++){
             if(st.empty()){
                 nsil.push_back(-1);
                 st.push({arr[i],i});
@@ -13,9 +13,8 @@ public:
                 nsil.push_back(st.top().second);
                 st.push({arr[i],i});
             }
-
             else{
-                while( !st.empty() && arr[i] < st.top().first){
+                while(!st.empty() && arr[i] < st.top().first ){
                     st.pop();
                 }
                 if(st.empty()) nsil.push_back(-1);
@@ -24,10 +23,10 @@ public:
             }
         }
     }
-
-    void func2(vector<int> &arr,vector<int>&nsir){
+    void func2(vector<int>arr, vector<int>&nsir){
         stack<pair<int,int>>st;
-        for(int i = arr.size()-1 ; i >= 0 ; i--){
+
+        for(int i = arr.size()-1; i >=0 ; i--){
             if(st.empty()){
                 nsir.push_back(arr.size());
                 st.push({arr[i],i});
@@ -36,9 +35,8 @@ public:
                 nsir.push_back(st.top().second);
                 st.push({arr[i],i});
             }
-
             else{
-                while( !st.empty() && arr[i] <= st.top().first){
+                while(!st.empty() && arr[i] <= st.top().first){
                     st.pop();
                 }
                 if(st.empty()) nsir.push_back(arr.size());
@@ -49,26 +47,15 @@ public:
         reverse(nsir.begin(),nsir.end());
     }
     int sumSubarrayMins(vector<int>& arr) {
-        int sum  = 0 ; 
-        
-
-        vector<int>nsil ;
-        vector<int>nsir ;
+        vector<int> nsil;
+        vector<int> nsir;
         func1(arr,nsil);
         func2(arr,nsir);
-
-
-        for(int i = 0 ; i < arr.size() ; i++){
-            cout << nsir[i] << " ";
-        }
-        for(int i = 0 ; i< arr.size();  i++){
+        int sum =0 ;
+        for(int i = 0 ; i< arr.size(); i++){
             int l = i - nsil[i];
             int r = nsir[i] - i ;
-
             sum = (long long)(sum % mod) + (long long)((arr[i] %mod ) * (long long)((l%mod) *  (long long)(r %mod))%mod )% mod;
-
-
-            
         }
         return sum ;
     }
