0ecbadf03c54a15db27e4a8b24d98631d5342bea
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-09-10 20:29:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (38.85%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
 
-    int climbdp(int ind , vector<int> &dp){
-        if(ind == 1) return 1 ;
-        if(ind == 2) return 2 ;
+    int climbdp(int ind, vector<int>&dp){
+        if(ind == 1 || ind == 2) return ind ;
 
-        if(dp[ind] != -1) return dp[ind];
-
+        if(dp[ind] != -1)return dp[ind];
         int left = climbdp(ind-1,dp);
         int right = climbdp(ind-2,dp);
 
-        return dp[ind] = left+ right ;
+        return dp[ind] = left+right ;
     }
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        
-        //return climbdp(n,dp);
-        for(int ind = 1 ; ind <= n ; ind++){
-            if(ind == 1){
-                dp[ind] = 1 ;
-                continue ;
-            }
-            if(ind == 2) {
-                dp[ind] = 2 ;
-                continue ;
-            }
-            //if(dp[ind] != -1) return dp[ind];
-
-            int left = dp[ind-1];
-            int right =dp[ind-2];
-
-            dp[ind] = left+ right ;
-        }
-
-        return dp[n];
+        return climbdp(n,dp);
     }
 };
