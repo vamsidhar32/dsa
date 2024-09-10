3a20be9bf5dcf1cb87c9f3c34dd71eed0caf98aa
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-09-10 15:27:48 +0530

    Time: 22 ms (15.90%) | Memory: 17.7 MB (48.26%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = numCourses;
-        vector<vector<int>> pre = prerequisites;
-
-        vector<int>adj[n];
-        vector<int>indegree(n);
-        for(int i = 0 ; i <pre.size(); i++){
-            adj[pre[i][0]].push_back(pre[i][1]);
-            indegree[pre[i][1]]++;
+        vector<int>ans ;
+        vector<int> adj[numCourses];
+        vector<int>indegree(numCourses);
+        for(auto it: prerequisites){
+            adj[it[0]].push_back(it[1]);
+            indegree[it[1]]++;
         }
-
-        
-
-        // for(int i = 0 ; i <n ; i++){
-        //     for(auto it :adj[i]){
-        //         indegree[it]++;
-        //     }
-        // }
         queue<int>q;
-        for(int i =0 ; i <n ; i++){
-            if(indegree[i] == 0 ) q.push(i);
+        for(int i =0 ; i<numCourses; i++ ){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
         }
 
-        vector<int>ans;
 
         while(!q.empty()){
-            int top = q.front();
+            auto top=  q.front();
             q.pop();
             ans.push_back(top);
-            for(auto it :adj[top]){
+            for(auto it : adj[top]){
                 indegree[it]--;
                 if(indegree[it] == 0) q.push(it);
             }
         }
+
         reverse(ans.begin(),ans.end());
-        if(ans.size() == n) return ans ;
+        if(ans.size() == numCourses) return ans ;
         return {};
+
     }
 };
