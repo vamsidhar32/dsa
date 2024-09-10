aa9ede3d2062aa324802303220e249682eef9849
Vamsidhar Reddy <110768857+vamsidhar32@users.noreply.github.com>
2024-09-10 11:25:02 +0530

    Time: 4 ms (84.79%) | Memory: 17.2 MB (50.56%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -26,8 +26,27 @@ public:
         int inicolor = image[sr][sc] ;
         vector<int>delrow = {0,0,-1,1};
         vector<int>delcol = {-1,1,0,0};
-        dfs(sr,sc,color,inicolor,image,n,m,delrow,delcol);
+        //dfs(sr,sc,color,inicolor,image,n,m,delrow,delcol);
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        image[sr][sc] = color ;
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
 
+            int row = it.first ;
+            int col = it.second ;
+            for(int i = 0 ; i < 4 ; i++){
+                int newrow = row+delrow[i];
+                int newcol = col+delcol[i];
+                if(newrow <n && newrow >=0 && newcol <m && newcol >=0){
+                    if(image[newrow][newcol] == inicolor){
+                        image[newrow][newcol] = color ;
+                        q.push({newrow,newcol});
+                    }
+                }
+            }
+        }
         return image;
     }
 };
